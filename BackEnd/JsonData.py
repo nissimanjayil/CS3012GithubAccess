990ffd9cea0433b6b510d50185641d6cfe1b7b8d
Nissi <nissimanjayil@gmail.com>
2019-11-02 13:21:49 +0000

    Parsed JSON

--- BackEnd/JsonData.py
+++ BackEnd/JsonData.py
@@ -1,56 +1,59 @@
 import requests;
 import json;
-
+import csv;
 
 base_url= 'https://api.github.com'
 
 
-def get_data(username,repo):
-    commit_history = get_commit(username,repo)
 
-
+#This function parses the commits from JSON data returned from the API request
 def get_commit(username,repo):
     url = '{}/repos/{}/{}/commits'.format(base_url,username,repo)
-    r = requests.get(url)
-    json_data = r.json()
-    json_parsed = json.loads(json_data)
-    print(json_parsed)
-
-
-
-
-# def get_Request(username,repo,sha):
-#     first_commit = get_commit(username,repo)
+    response = requests.get(url)
+    print(response.status_code)
+    if response.status_code == 200:
+        print("Connection successful.Decoding JSON")
+    json_data = response.json()
+    json_commit = json_data[0]['commit']
+   
+    return json_commit
     
-#     # compare_url = '{}/repos/{}/{}/compare/{}...{}'.format(base_url,id,repo,first_commit,sha)
-
-#     # commit_req = requests.get(base_url)
-#     # commit_count = commit_req.json()['total_commits'] + 1
-#     # print(commit_count)
-#     # return commit_count
-
-
-# def get_commit(username,repo):
     
-
-#     # if r.headers.get('Link'):
-#     #     page_url = req.headers.get('Link').split(',')[1].split(';')[0].split('<')[1].split('>')[0]
-#     #     req_last_commit = requests.get(page_url)
-#     #     first_commit = req_last_commit.json()
-#     #     first_commit_hash = first_commit[-1]['sha']
-#     # else:
-#     #     first_commit_hash = json_data[-1]['sha']
-
-#     # return first_commit_hash
-
+# url = base_url
+# json_data = requests.get(url).json()
+# json_commit = json_data[0]['commit']
+# print(json_commit)
+    
 
 def main():
     username = 'torvalds'
     repo = 'Linux'
    
-
     data = get_commit(username,repo)
     print(data)
+      
+   
+    # field_names =[array1,
+
+    # ]
+
+    # output_file = open('csv_file.csv',"w")
+    # output = csv.DictWriter(output_file,
+    #             fieldnames=field_names,
+    #             restval=None,
+    #             extrasaction='ignore',
+    #             delimiter='|',
+    #             quoting=csv.QUOTE_ALL,
+    #             quotechar='"',
+    #             doublequote=True,  
+    #             lineterminator='\n')    
+    
+    # for row in data:
+    #     output.writerow(row)
+   
+   
+ #https://api.github.com/repos/torvalds/Linux/commits
+    
 
 
 
