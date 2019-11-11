2893fed6e716476a4a719465c62df21d149bece6
Nissi <nissimanjayil@gmail.com>
2019-11-11 12:27:43 +0000

    Angular Parse Commits

--- Data-Visualisation/src/app/data.service.ts
+++ Data-Visualisation/src/app/data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,9 +10,12 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
+  private URL = "https://api.github.com/repos/torvalds/Linux/commits"
+
   getRemoteData(){
-    return this.http.get(
-      "https://api.github.com/repos/torvalds/Linux/commits"
-    );
+    return this.http.get(this.URL);
+   
+
   }
+ 
 }
--- Data-Visualisation/src/app/request/request.component.ts
+++ Data-Visualisation/src/app/request/request.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DataService} from "../data.service";
 
+
 @Component({
   selector: 'app-request',
   templateUrl: './request.component.html',
@@ -8,14 +9,36 @@ import {DataService} from "../data.service";
 })
 
 export class RequestComponent implements OnInit {
-
+  
+   arrayData:Array<object>=[];
+   filteredCommits:Array<object>=[];
+ 
   constructor(private dataService:DataService) { }
-
+ 
   ngOnInit() {
-    this.dataService.getRemoteData().subscribe(data=>{
-      console.log("remote data");
-      console.log(data);
-    })
+    
+    this.dataService.getRemoteData().subscribe((data:Array<Object>=[])=>{   
+      this.arrayData = data; 
+      var k =0;
+      var i=0;
+     for(var j =0 ;j<this.arrayData.length;j++){
+      this.filteredCommits[j] = this.arrayData[i]['commit']
+      console.log("Filtered Commits")
+      console.log(this.filteredCommits[k])
+      i++;
+      k++;
+    }
+    }); 
+    
+   
+  
   }
 
+  
+
 }
+
+  
+
+
+
